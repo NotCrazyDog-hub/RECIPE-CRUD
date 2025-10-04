69ca638b7168d88121085b12f3170b484c341fe9
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-10-04 13:44:15 -0300

    Formulário de delete criado

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -3,7 +3,12 @@
 <ul>
     @foreach ($recipes as $recipe)
         <li>
-            {{ $recipe->id }} - {{ $recipe->food_name }} | <a href="{{ route('recipes.edit', ['recipe' => $recipe->id]) }}">Editar</a>
+            {{ $recipe->id }} - {{ $recipe->food_name }} | <a href="{{ route('recipes.edit', ['recipe' => $recipe->id]) }}">Editar</a> |
+            <form action="{{ route('recipes.destroy', ['recipe' => $recipe->id]) }}" method="post">
+                @csrft
+                @method('DELETE')
+                <input type="submit" onclick="return confirm('Tem certeza em deletar esta receita de sua lista?')" value="Deletar">
+            </form>
         </li>
     @endforeach
 </ul>
