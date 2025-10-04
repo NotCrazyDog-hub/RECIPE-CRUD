ee2420d059f8bb5fbd9991e1373c8ddf1cc5b0cc
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-10-04 12:14:58 -0300

    Importação do RecipeController e criação da rota recipes.index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.index');
