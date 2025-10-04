10b37cd68022208e0e5e9aa70495805b4cc71912
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-10-04 13:41:18 -0300

    Rota 'recipes.destroy' criada

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Route::get('/recipes/create', [RecipeController::class, 'create'])->name('recipe
 Route::post('/recipes', [RecipeController::class, 'store'])->name('recipes.store');
 Route::get('/recipes/{recipe}/edit', [RecipeController::class, 'edit'])->name('recipes.edit');
 Route::put('/recipes/{recipe}', [RecipeController::class, 'update'])->name('recipes.update');
+Route::delete('/recipes/{recipe}', [RecipeController::class, 'destroy'])->name('recipes.destroy');
