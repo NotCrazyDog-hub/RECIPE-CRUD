1b61b81f578eb10f9536025421064d4a829ebd70
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-10-04 13:27:13 -0300

    Rota 'recipes.update' criada

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.index'
 Route::get('/recipes/create', [RecipeController::class, 'create'])->name('recipes.create');
 Route::post('/recipes', [RecipeController::class, 'store'])->name('recipes.store');
 Route::get('/recipes/{recipe}/edit', [RecipeController::class, 'edit'])->name('recipes.edit');
+Route::put('/recipes/{recipe}', [RecipeController::class, 'update'])->name('recipes.update');
