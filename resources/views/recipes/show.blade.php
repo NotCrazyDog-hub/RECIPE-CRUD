5935d854d7124fc7981137d1abb693436a51d88f
NotCrazyDog-hub <pedro.oliveira.ncd@gamil.com>
2025-10-04 22:37:51 -0300

    Estrutura básica de show.blade.php criada

---
+++ resources/views/recipes/show.blade.php
@@ -0,0 +1,10 @@
+<h1>Como fazer {{ $recipe->food_name }}</h1>
+
+<h4>Ingredientes:</h4>
+<p>{{ $recipe->ingredients }}</p>
+
+
+<h4>Passo a passo:</h4>
+<p>{{ $recipe->instructions }}</h4>
+
+<h4>Tempo de preparo: {{ $recipe->prep_time }} minutos</h4>
